f08d8fe59fc7acb339ca8155d1885e24f46578ab
liRemons <remons@foxmail.com>
2021-11-07 14:15:57 +0800

    fix:修复路由无法打开的问题

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,12 @@
 import Demo from '@pages/demo'
 export const routerConfig = [
   { title: '首页', path: '/demo', component: Demo, type: 'router' },
-  { title: 'nav1', path: 'http://remons.cn:8001/demo', type: 'iframe' },
+  { title: 'nav1', path: 'http://remons.cn:8001/demo/index.html', type: 'iframe' },
   {
     title: 'subNav',
     path: 'subNav',
     children: [
-      { title: 'subNav1', path: 'http://remons.cn:8001/demo1', type: 'iframe' }
+      { title: 'subNav1', path: 'http://remons.cn:8001/demo1/index.html', type: 'iframe' }
     ]
   }
 ]
